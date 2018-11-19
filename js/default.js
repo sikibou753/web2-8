38ae7469e0d2a987af662489cbcd6cb602cd41af
sikibou753 <38420596+sikibou753@users.noreply.github.com>
2018-11-19 17:45:36 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -17,24 +17,58 @@ if(filename === 'other.html'){
   return [sec,min,hours,days];
 }
 
-var update = function(){
-  var now =new Date();
-  var target =new Date(2020,7,24,0,0,0,0);
-  var diff =target.getTime() -now.getTime();
-  var counter =separate_time(diff);
-  document.getElementById('countdown').textContent=
-    '東京オリンピックまであと　'+
-    counter[3] + '日' + 
-    counter[2] + '時間' + 
-    counter[1] + '分' + 
-    counter[0] + '秒' ;
-refresh();
-}
+    var update = function(){
+      var now =new Date();
+      var target =new Date(2020,7,24,0,0,0,0);
+      var diff =target.getTime() -now.getTime();
+      var counter =separate_time(diff);
+      document.getElementById('countdown').textContent=
+        '東京オリンピックまであと　'+
+        counter[3] + '日' + 
+        counter[2] + '時間' + 
+        counter[1] + '分' + 
+        counter[0] + '秒' ;
+    refresh();
+    }
 
-var refresh=function(){
-  setTimeout(update,1000);
-}
-update();
+    var refresh=function(){
+      setTimeout(update,1000);
+    }
+    update();
+  
+    fuction setCookie(c_name,value,expiredays){
+      var extiee=new Date().getTime();
+      var cltime=new Date(extime+(60*60*24*1000*expiredays));
+      var exdate=cltime.toUTCString();
+      
+      var s="";
+      s+=c_name+"="+ escape(value);
+      s+= "; path="+lcation.pathname;
+      if(expiredays){
+        s+= "; expires=" +exdate+"; ";
+      }else{
+        s+= "; ";
+      }
+      
+      document.cookie=s;
+    }
+  
+    function getCookie(c_name){
+      var st="";
+      var ed="";
+      if(0<document.cookie.lenght){
+        st=document.cookie.indexOf(c_name + "=");
+        if(st!=-1){
+          st=st+c_name.length+1;
+          ed=document.cookie.indexOf(";",st);
+          if(ed==-1)ed=document.conkie.length;
+          
+          return unescape(document.cookie.substring(st,ed));
+        }
+      }
+      return "";
+    }
+    
 }
 opt.selected=true;
 

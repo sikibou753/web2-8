352a58f9a8a01766849fd915e5ff82a336c2e0f7
sikibou753 <38420596+sikibou753@users.noreply.github.com>
2018-11-19 18:06:14 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -9,7 +9,36 @@ if(filename === 'other.html'){
 }else{
   opt=document.querySelector('option[value="index.html"]');
   
-  function setCookie(c_name,value,expiredays){
+  var separate_time =function(time){
+  var sec =Math.floor((time/1000)%60);
+  var min =Math.floor((time/1000/60)%60);
+  var hours =Math.floor((time/1000/60/60)%24);
+  var days =Math.floor(time/1000/60/60/24);
+  return [sec,min,hours,days];
+}
+
+    var update = function(){
+      var now =new Date();
+      var target =new Date(2020,7,24,0,0,0,0);
+      var diff =target.getTime() -now.getTime();
+      var counter =separate_time(diff);
+      document.getElementById('countdown').textContent=
+        '東京オリンピックまであと　'+
+        counter[3] + '日' + 
+        counter[2] + '時間' + 
+        counter[1] + '分' + 
+        counter[0] + '秒' ;
+    refresh();
+    }
+
+    var refresh=function(){
+      setTimeout(update,1000);
+    }
+    update();
+}
+opt.selected=true;
+
+function setCookie(c_name,value,expiredays){
       var extiee=new Date().getTime();
       var cltime=new Date(extime+(60*60*24*1000*expiredays));
       var exdate=cltime.toUTCString();
@@ -55,35 +84,6 @@ if(filename === 'other.html'){
     document.getElementById('remove_cookie').onsubmit=function(){
       setCookie('lastDate',"",0);
     }
-  
-  var separate_time =function(time){
-  var sec =Math.floor((time/1000)%60);
-  var min =Math.floor((time/1000/60)%60);
-  var hours =Math.floor((time/1000/60/60)%24);
-  var days =Math.floor(time/1000/60/60/24);
-  return [sec,min,hours,days];
-}
-
-    var update = function(){
-      var now =new Date();
-      var target =new Date(2020,7,24,0,0,0,0);
-      var diff =target.getTime() -now.getTime();
-      var counter =separate_time(diff);
-      document.getElementById('countdown').textContent=
-        '東京オリンピックまであと　'+
-        counter[3] + '日' + 
-        counter[2] + '時間' + 
-        counter[1] + '分' + 
-        counter[0] + '秒' ;
-    refresh();
-    }
-
-    var refresh=function(){
-      setTimeout(update,1000);
-    }
-    update();
-}
-opt.selected=true;
 
 document.getElementById('form').select.onchange=function(){
   location.href=document.getElementById('form').select.value;
